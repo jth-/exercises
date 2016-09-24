52f1618a263f2c1e8c5067cb25c6562c7d1a6149
Jimmy Harrison <jtharrison91@gmail.com>
2016-09-24 17:46:04 -0400

    Currently supporting array $push operation as an initial pass. Right now, maintaining a logical separation between arrays and objects because of the mutual exclusivity of the operations.

--- update/update.js
+++ update/update.js
@@ -8,16 +8,30 @@ function update(state, commands) {
 }
 
 function updateArray(state, commands) {
-  return new Array();
+  // Use slice to create a clone of an array
+  var newState = state.slice(0);
+  // Under the assumption that we don't support nested operations (like $set)
+  // within the elements of an array, so we can trust the keys in the command
+  // object to be actual array commands. Otherwise, we'll throw an exception.
+  Object.keys(commands).forEach(function(key) {
+    if (!arrayCommands.hasOwnProperty(key)) {
+      throw "Command doesn't exist for arrays - " + key;
+    }
+    arrayCommands[key](newState, commands[key]);
+  });
+  return newState;
 }
 
 var arrayCommands = {
-  $push: function() {},
+  $push: function(state, values) {
+    state.push.apply(state, values);
+  },
   $unshift: function() {},
   $splice: function() {}
 };
 
 function updateObject(state, commands) {
+
   return {};
 }
 
