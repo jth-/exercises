6cc4ebc23f17dee33448876450c906c473cba471
Jimmy Harrison <jtharrison91@gmail.com>
2016-09-25 09:28:18 -0400

    Implement a portion of the transmitter that converts a message to an array of boolean values that represent whether the transmitter should toggle on a given tick

--- morse-code/transmitter.js
+++ morse-code/transmitter.js
@@ -1,3 +1,24 @@
 module.exports = function(options, callback) {
+  var toggleTimes = [];
+  // First, create a list of boolean values that decide whether, on any
+  // given tick, the toggle method should be executed or not.
+  for (var i = 0; i < options.message.length; i++) {
+    // Iterate through each character in the message and do a lookup
+    // on it's corresponding code.
+    var encoded = options.codes[options.message[i]] || ' ';
+    for (var j = 0; j < encoded.length; j++) {
+      // Iterate through each morse code symbol in the encoded character
+      // and do a lookup on it's corresponding toggleTimes, then push
+      // that to the whole message toggle times array
+      toggleTimes.push.apply(toggleTimes, letterToToggles[encoded[j]]);
+    }
+  }
+  console.log(toggleTimes);
   return callback();
+}
+
+var letterToToggles = {
+  '.': [ true, true, false ],
+  '-': [ true, false, false, true, false ],
+  ' ': [ false, false, false, false, false, false]
 }
