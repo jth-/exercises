3a4c6811d6b6534be2510a0b0bdce43f8769dca7
Jimmy Harrison <jtharrison91@gmail.com>
2016-09-25 10:43:03 -0400

    Fix issues with toggle times of multiple character words and multiple word sentences with some logic around properly prefixing padding to words, characters, and symbols. I treat the space as it's own symbol that has a toggle time of 7 ticks. All tests are passing now, and code is commented.

--- morse-code/transmitter.js
+++ morse-code/transmitter.js
@@ -1,38 +1,55 @@
 module.exports = function(options, callback) {
+  // Create a list of waiting times between toggle events for the message
   var toggleTimes = getToggleTimes(options.message, options.codes);
+  // Track where we are in the toggle times array
   var index = 0;
-  next();
-  function next() {
+  // Kick off the transmission of the message
+  nextToggle();
+
+  function nextToggle() {
+    // Execute the toggle operation
+    options.toggle();
     if (!toggleTimes[index]) {
+      // If there are no more toggles, invoke the final callback
       return callback();
     }
-    options.toggle();
+    // Wait for however long is prescribed by the toggle times array
     var waitFor = toggleTimes[index];
     index++;
-    options.timeouter(next, waitFor);
+    options.timeouter(nextToggle, waitFor);
   }
 };
 
 function getToggleTimes(message, codes) {
   var times = [];
-  // First, create a list of boolean values that decide whether, on any
-  // given tick, the toggle method should be executed or not.
   for (var i = 0; i < message.length; i++) {
     // Iterate through each character in the message and do a lookup
     // on it's corresponding code.
     var encoded = codes[message[i]] || ' ';
+    // Prefix each character that isn't the first character in
+    // the word, the first character following a space, or is an
+    // space with 3 ticks of padding
+    if (i !== 0 && message[i - 1] !== ' ' && encoded !== ' ') {
+      times.push(3);
+    }
     for (var j = 0; j < encoded.length; j++) {
+      // Prefix each symbol that isn't the first symbol in a character
+      // with 1 tick of padding
+      if (j !== 0) {
+        times.push(1);
+      }
       // Iterate through each morse code symbol in the encoded character
-      // and do a lookup on it's corresponding toggleTimes, then push
-      // that to the whole message toggle times array
-      times.push.apply(times, letterToToggles[encoded[j]]);
+      // and do a lookup on it's corresponding toggle ticks, then push
+      // that to the toggle times array
+      times.push(symbolToToggles[encoded[j]]);
     }
   }
   return times;
 }
 
-var letterToToggles = {
-  '.': [1, 1],
-  '-': [3, 1],
-  ' ': [7]
+// Lookup table for toggle durations for each morse code symbol
+var symbolToToggles = {
+  '.': 1,
+  '-': 3,
+  ' ': 7
 };
