2bb43c85942c54cd1590adb5cc302f9a7532dfdf
Jimmy Harrison <jtharrison91@gmail.com>
2016-09-25 09:31:18 -0400

    Move getToggleTimes method to its own method, just to separate concerns a bit. This would also allow you to test smaller chunks of the transmitter, if writing unit tests, and thus would improve testability

--- morse-code/transmitter.js
+++ morse-code/transmitter.js
@@ -1,20 +1,25 @@
 module.exports = function(options, callback) {
-  var toggleTimes = [];
+  var toggleTimes = getToggleTimes(options.message, options.codes);
+  console.log(toggleTimes);
+  return callback();
+}
+
+function getToggleTimes(message, codes) {
+  var times = [];
   // First, create a list of boolean values that decide whether, on any
   // given tick, the toggle method should be executed or not.
-  for (var i = 0; i < options.message.length; i++) {
+  for (var i = 0; i < message.length; i++) {
     // Iterate through each character in the message and do a lookup
     // on it's corresponding code.
-    var encoded = options.codes[options.message[i]] || ' ';
+    var encoded = codes[message[i]] || ' ';
     for (var j = 0; j < encoded.length; j++) {
       // Iterate through each morse code symbol in the encoded character
       // and do a lookup on it's corresponding toggleTimes, then push
       // that to the whole message toggle times array
-      toggleTimes.push.apply(toggleTimes, letterToToggles[encoded[j]]);
+      times.push.apply(times, letterToToggles[encoded[j]]);
     }
   }
-  console.log(toggleTimes);
-  return callback();
+  return times;
 }
 
 var letterToToggles = {
